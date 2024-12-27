ac09a65ec6512c171e6d8f65339df039bf7be443
Iffatuz Zahra <iffatuz.zahra.ft22@mail.umy.ac.id>
2024-12-27 16:32:04 +0700

    update

--- src/controllers/Controller_Login.js
+++ src/controllers/Controller_Login.js
@@ -17,34 +17,60 @@ module.exports = {
         });
     },
     loginAuth(req, res) {
-        let username = req.body.username;
-        let password = req.body.pass;
-        if (username && password) {
+        let usernameOrEmail = req.body.username;
+        let password = req.body.password;
+        let role = req.body.role;
+
+        if (usernameOrEmail && password) {
             pool.getConnection(function (err, connection) {
                 if (err) throw err;
-                connection.query(
-                    `SELECT * FROM admin WHERE username = ? AND password = SHA2(?,512)`
-                    , [username, password], function (error, results) {
-                        if (error) throw error;
-                        if (results.length > 0) {
-                            req.session.loggedin = true;
-                            req.session.userid = results[0].id;
-                            req.session.username = results[0].nama;
+
+                let query = '';
+                let params = [];
+
+                if (role === 'admin') {
+                    query = `SELECT * FROM admin WHERE username = ? AND password = SHA2(?,512)`;
+                    params = [usernameOrEmail, password];
+                } else if (role === 'user') {
+                    query = `SELECT * FROM user WHERE email = ? AND pw_user = SHA2(?,512)`;
+                    params = [usernameOrEmail, password];
+                }
+
+                connection.query(query, params, function (error, results) {
+                    if (error) throw error;
+
+                    if (results.length > 0) {
+                        req.session.loggedin = true;
+                        req.session.userid = results[0].id || results[0].id_user;
+                        req.session.username = results[0].username || results[0].email;
+                        req.session.role = role;
+
+                        if (role === 'admin') {
                             res.redirect('/');
-                        } else {
-                            req.flash('color', 'danger');
-                            req.flash('status', 'Oops..');
-                            req.flash('message', 'Akun tidak ditemukan');
-                            res.redirect('/login');
+                        } else if (role === 'user') {
+                            res.redirect('/home_user');
                         }
-                    });
+                    } else {
+                        req.flash('color', 'danger');
+                        req.flash('status', 'Oops..');
+                        req.flash('message', 'Akun tidak ditemukan');
+                        res.redirect('/login');
+                    }
+
+                });
+
                 connection.release();
-            })
+            });
+
         } else {
+            req.flash('color', 'danger');
+            req.flash('status', 'Oops..');
+            req.flash('message', 'Username/Email dan Password wajib diisi!');
             res.redirect('/login');
             res.end();
         }
     },
+
     logout(req, res) {
         req.session.destroy((err) => {
             if (err) {
