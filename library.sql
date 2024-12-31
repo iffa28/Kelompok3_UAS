b6eeeb0783438df9c3c40221f0e1f8044f857e60
Iffatuz Zahra <iffatuz.zahra.ft22@mail.umy.ac.id>
2024-12-31 13:08:03 +0700

    Co-authored-by: Zahro03 <Zahro03@users.noreply.github.com> Co-authored-by: namira121 <namira121@users.noreply.github.com> Co-authored-by: Safirasn21 <Safirasn21@users.noreply.github.com> Co-authored-by: andiniseptia <andiniseptia@users.noreply.github.com> Co-authored-by: Ulfah <ulfhiraa@users.noreply.github.com>

--- library.sql
+++ library.sql
@@ -22,10 +22,13 @@ CREATE TABLE `buku` (
 );
 
 CREATE TABLE `peminjaman` (
-    `id_peminjaman` int(8) NOT NULL AUTO_INCREMENT,
-    `id_buku` int(8) FOREIGN KEY,
-    `id_user` int(8) FOREIGN KEY,
-    `judul_buku` varchar(50) DEFAULT NULL,
-    `tgl_pinjam` date DEFAULT NULL,
-    `tgl_kembali` date DEFAULT NULL
-)
+    `id_peminjaman` INT(8) NOT NULL AUTO_INCREMENT,
+    `id_buku` INT(8) NOT NULL,
+    `id_user` INT(8) NOT NULL,
+    `judul_buku` VARCHAR(50) DEFAULT NULL,
+    `tgl_pinjam` DATE DEFAULT NULL,
+    `tgl_kembali` DATE DEFAULT NULL,
+    PRIMARY KEY (`id_peminjaman`),
+    FOREIGN KEY (`id_buku`) REFERENCES `buku`(`id_buku`) ON DELETE CASCADE ON UPDATE CASCADE,
+    FOREIGN KEY (`id_user`) REFERENCES `user`(`id_user`) ON DELETE CASCADE ON UPDATE CASCADE
+) 
